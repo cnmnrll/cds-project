3e3efcf4553c042fb7d888429892cf93574d2472
xueqing <lixueqing07@gmail.com>
2024-04-01 18:24:18 +0800

    import spline model

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,11 @@ import ExplorePage from "./pages/ExplorePage";
 import HomePage from "./pages/HomePage";
 
 function App() {
+  window.onload = function () {
+    var shadowRoot = document.querySelector("spline-viewer").shadowRoot;
+    shadowRoot.querySelector("#logo").remove();
+  };
+
   const router = createBrowserRouter([
     {
       path: "/",
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,3 +1,13 @@
+import bg from "../assets/bg.png";
+
 export default function HomePage() {
-  return <div>homepage</div>;
+  return (
+    <div className="w-full h-screen relative">
+      <img src={bg} alt="bg" className="w-full h-auto" />
+      <div className="z-10 absolute top-0 left-0 w-full h-full">
+        <p className="font-medium text-xl text-white">Bloobin</p>
+        <spline-viewer url="https://prod.spline.design/378qG2pHeQuxFSdZ/scene.splinecode"></spline-viewer>
+      </div>
+    </div>
+  );
 }
