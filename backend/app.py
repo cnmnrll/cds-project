c39018e108dc071ed2ed2c26b3d7aea84277f6c3
Huy Nguyen <edwardhaythamconnor@gmail.com>
2024-04-02 16:54:26 +0800

    Add model to backend

--- backend/app.py
+++ backend/app.py
@@ -1,43 +1,38 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from keras.models import load_model
-from keras.preprocessing import image
+import keras.utils as image
 import numpy as np
+from PIL import Image
 
 app = Flask(__name__)
+CORS(app)
 
 # Load the trained model
-model = load_model('image_classification_model.h5')
+model_path = 'models_h5/resnet50.h5'
+model = load_model(model_path)
+
+class_labels = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
 
 @app.route('/classify', methods=['POST'])
 def classify():
-    # Get the image file from the request
+    if 'image' not in request.files:
+        print('No file part')
+        return jsonify({'error': 'No file part'})
+
     image_file = request.files['image']
-
-    img = image.load_img(image_file, target_size=(64, 64))
-
-    # Convert the image to a numpy array
-    img_array = image.img_to_array(img)
-
-    # Expand dimensions to match the input shape of the model
+    #img = image.load_img(image_file, target_size=(64, 64))
+    img = Image.open(image_file)
+    img = img.resize((300, 300)) 
+    img = img.convert('RGB')
+    img_array = np.array(img)
+    img_array = img_array / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
-    # Normalize the image
-    img_array = img_array / 255.0
-
-    # Make predictions
     prediction = model.predict(img_array)
-
-    # Get the predicted class
     predicted_class = np.argmax(prediction)
-
-    # Define class labels
-    class_labels = ['glass', 'metal', 'paper']
-
-    # Get the corresponding label
     predicted_label = class_labels[predicted_class]
-
-    # Return the result as JSON
     return jsonify({'prediction': predicted_label})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000, debug=True)
