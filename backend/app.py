ed0cff5411fe8631be8fee560cde7f43fac9675d
Huy Nguyen <edwardhaythamconnor@gmail.com>
2024-04-05 18:52:02 +0800

    Fix backend model bug

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,9 @@ from keras.models import load_model
 import keras.utils as image
 import numpy as np
 from PIL import Image
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from io import BytesIO
 
 app = Flask(__name__)
 CORS(app)
@@ -12,27 +15,28 @@ CORS(app)
 model_path = 'models_h5/resnet50.h5'
 model = load_model(model_path)
 
-class_labels = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
+class_labels = ['cardboard', 'glass', 'paper', 'metal',  'plastic', 'trash']
 
 @app.route('/classify', methods=['POST'])
 def classify():
-    if 'image' not in request.files:
-        print('No file part')
-        return jsonify({'error': 'No file part'})
+    try:       
+        image_file = request.files['image']
+        img_content = image_file.read()
+        img_byte = BytesIO(img_content)
 
-    image_file = request.files['image']
-    #img = image.load_img(image_file, target_size=(64, 64))
-    img = Image.open(image_file)
-    img = img.resize((300, 300)) 
-    img = img.convert('RGB')
-    img_array = np.array(img)
-    img_array = img_array / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
+        # Image Preprocessing
+        img = image.load_img(img_byte, target_size=(300, 300))
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
 
-    prediction = model.predict(img_array)
-    predicted_class = np.argmax(prediction)
-    predicted_label = class_labels[predicted_class]
-    return jsonify({'prediction': predicted_label})
+        # Image Prediction
+        prediction = model.predict(img_array)
+        predicted_class = np.argmax(prediction)
+        predicted_label = class_labels[predicted_class]
+        return jsonify({'prediction': predicted_label})
+    except Exception as e:
+        return jsonify({'error': 'Error retrieving image'})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000, debug=True)
