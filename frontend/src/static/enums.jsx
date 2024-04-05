f701f848a355eef573f4326b99c2a39f224df5e8
xueqing <lixueqing07@gmail.com>
2024-04-05 14:42:06 +0800

    add waste info enum

--- frontend/src/static/enums.jsx
+++ frontend/src/static/enums.jsx
@@ -12,12 +12,13 @@ export const wastes = [
     image: glass,
     about: (
       <>
-        Ullamco qui velit deserunt non sit culpa. Quis ut mollit qui incididunt
-        aliqua non sit. Sint ullamco nisi consequat excepteur non nisi magna ea
-        tempor elit qui proident magna velit. Laborum cupidatat eu voluptate
-        aliquip eu nulla ea incididunt voluptate veniam. Amet eu adipisicing
-        proident labore officia est nisi magna qui fugiat deserunt eiusmod eu
-        labore.
+        Glass waste is a significant environmental concern due to its
+        non-biodegradable nature. When disposed of improperly, glass contributes
+        to pollution and poses risks to wildlife and ecosystems. Recycling glass
+        is crucial to mitigate these impacts, as it reduces energy consumption
+        and raw material extraction. Additionally, recycled glass can be used in
+        various industries, promoting sustainability and resource conservation
+        efforts worldwide.
         <br />
         <br />
         Consectetur culpa cillum aute mollit commodo pariatur nulla aliqua
@@ -60,12 +61,13 @@ export const wastes = [
     image: paper,
     about: (
       <>
-        Ullamco qui velit deserunt non sit culpa. Quis ut mollit qui incididunt
-        aliqua non sit. Sint ullamco nisi consequat excepteur non nisi magna ea
-        tempor elit qui proident magna velit. Laborum cupidatat eu voluptate
-        aliquip eu nulla ea incididunt voluptate veniam. Amet eu adipisicing
-        proident labore officia est nisi magna qui fugiat deserunt eiusmod eu
-        labore.
+        Paper waste is a prevalent issue globally, with millions of tons
+        discarded annually. Improper disposal leads to landfills overflowing and
+        contributes to deforestation. Recycling paper is crucial, as it
+        conserves natural resources, reduces energy consumption, and minimizes
+        pollution. By utilizing recycled paper products and promoting
+        sustainable practices, individuals and businesses can significantly
+        reduce their environmental footprint and support a healthier planet.
         <br />
         <br />
         Consectetur culpa cillum aute mollit commodo pariatur nulla aliqua
@@ -108,12 +110,13 @@ export const wastes = [
     image: cardboard,
     about: (
       <>
-        Ullamco qui velit deserunt non sit culpa. Quis ut mollit qui incididunt
-        aliqua non sit. Sint ullamco nisi consequat excepteur non nisi magna ea
-        tempor elit qui proident magna velit. Laborum cupidatat eu voluptate
-        aliquip eu nulla ea incididunt voluptate veniam. Amet eu adipisicing
-        proident labore officia est nisi magna qui fugiat deserunt eiusmod eu
-        labore.
+        Cardboard waste poses a significant environmental challenge due to its
+        abundance and slow decomposition rate. Improper disposal fills landfills
+        and contributes to pollution. Recycling cardboard is essential for
+        conserving resources, reducing energy consumption, and minimizing
+        environmental impact. By reusing and recycling cardboard packaging,
+        businesses and individuals can help alleviate strain on ecosystems and
+        promote a more sustainable future.
         <br />
         <br />
         Consectetur culpa cillum aute mollit commodo pariatur nulla aliqua
@@ -156,12 +159,13 @@ export const wastes = [
     image: plastic,
     about: (
       <>
-        Ullamco qui velit deserunt non sit culpa. Quis ut mollit qui incididunt
-        aliqua non sit. Sint ullamco nisi consequat excepteur non nisi magna ea
-        tempor elit qui proident magna velit. Laborum cupidatat eu voluptate
-        aliquip eu nulla ea incididunt voluptate veniam. Amet eu adipisicing
-        proident labore officia est nisi magna qui fugiat deserunt eiusmod eu
-        labore.
+        Plastic waste presents a pressing global crisis, with millions of tons
+        entering landfills and oceans annually. Its persistence in the
+        environment for hundreds of years poses grave threats to wildlife and
+        ecosystems. Recycling plastic is crucial for reducing pollution and
+        conserving resources. However, effective waste management policies and
+        widespread adoption of sustainable alternatives are necessary to combat
+        this escalating issue.
         <br />
         <br />
         Consectetur culpa cillum aute mollit commodo pariatur nulla aliqua
@@ -204,12 +208,13 @@ export const wastes = [
     image: metal,
     about: (
       <>
-        Ullamco qui velit deserunt non sit culpa. Quis ut mollit qui incididunt
-        aliqua non sit. Sint ullamco nisi consequat excepteur non nisi magna ea
-        tempor elit qui proident magna velit. Laborum cupidatat eu voluptate
-        aliquip eu nulla ea incididunt voluptate veniam. Amet eu adipisicing
-        proident labore officia est nisi magna qui fugiat deserunt eiusmod eu
-        labore.
+        Metal waste is a significant concern due to its environmental and
+        economic impacts. Improper disposal leads to pollution and resource
+        depletion. Recycling metal is vital for conserving natural resources,
+        reducing energy consumption, and mitigating greenhouse gas emissions. By
+        promoting recycling programs and incorporating recycled metal into
+        manufacturing processes, industries and individuals can contribute to a
+        more sustainable and circular economy.
         <br />
         <br />
         Consectetur culpa cillum aute mollit commodo pariatur nulla aliqua
@@ -252,12 +257,14 @@ export const wastes = [
     image: trash,
     about: (
       <>
-        Ullamco qui velit deserunt non sit culpa. Quis ut mollit qui incididunt
-        aliqua non sit. Sint ullamco nisi consequat excepteur non nisi magna ea
-        tempor elit qui proident magna velit. Laborum cupidatat eu voluptate
-        aliquip eu nulla ea incididunt voluptate veniam. Amet eu adipisicing
-        proident labore officia est nisi magna qui fugiat deserunt eiusmod eu
-        labore.
+        General trash, comprising various materials like food waste, textiles,
+        and non-recyclable plastics, poses substantial environmental challenges.
+        Improper disposal results in overflowing landfills, pollution, and
+        habitat destruction. Effective waste management strategies, including
+        composting, incineration, and landfill diversion, are essential for
+        minimizing these impacts. Additionally, public awareness campaigns and
+        policies promoting waste reduction and recycling are crucial for
+        fostering a more sustainable future.
         <br />
         <br />
         Consectetur culpa cillum aute mollit commodo pariatur nulla aliqua
